4257af68d7a9224b737d9f4bea17a05b1690e082
EvanKang <xxtora0522@gmail.com>
2024-09-29 21:32:48 +0900

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+function Square() {
+  return <button className="square">1</button>;
+}
+
+export default function Board(){
+  return (
+    <> 
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
